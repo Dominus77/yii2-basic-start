27da136c456a8774c4618bf95b5db54fb2420b0f
Alexey Schevchenko <ivanovosity@gmail.com>
2018-02-15 21:48:32 +0300

    add yii_test

--- config/console.php
+++ config/console.php
@@ -4,6 +4,14 @@ return [
     'id' => 'app-console',
     'language' => 'en',
     'controllerNamespace' => 'app\commands',
+    'controllerMap' => [
+        'migrate' => [
+            'class' => 'yii\console\controllers\MigrateController',
+            'migrationNamespaces' => [
+                'modules\user\migrations',
+            ],
+        ],
+    ],
     'components' => [
     ],
 ];
--- config/test.php
+++ config/test.php
@@ -34,9 +34,6 @@ return [
     'components' => [
         'db' => [
             'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=localhost;dbname=yii2basic_start_test',
-            'username' => 'root',
-            'password' => '',
             'charset' => 'utf8',
         ],
         'mailer' => [
