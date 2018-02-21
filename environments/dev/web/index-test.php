89e97ddd6915b7e1688eb26e69090b9b5da8d8ef
Alexey Schevchenko <ivanovosity@gmail.com>
2018-02-21 21:27:46 +0300

    commit

--- environments/dev/web/index-test.php
+++ environments/dev/web/index-test.php
@@ -8,9 +8,9 @@ if (!in_array(@$_SERVER['REMOTE_ADDR'], ['127.0.0.1', '::1'])) {
 defined('YII_DEBUG') or define('YII_DEBUG', true);
 defined('YII_ENV') or define('YII_ENV', 'test');
 
-require __DIR__ . '/../vendor/autoload.php';
-require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
-require __DIR__ . '/../config/bootstrap.php';
+require(__DIR__ . '/../vendor/autoload.php');
+require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
+require(__DIR__ . '/../config/bootstrap.php');
 
 $config = yii\helpers\ArrayHelper::merge(
     require(__DIR__ . '/../config/common.php'),
