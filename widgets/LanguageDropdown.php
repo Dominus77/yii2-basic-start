392013970c438ed044bda9a1999d105f69dcac53
Alexey Schevchenko <ivanovosity@gmail.com>
2018-02-23 03:02:26 +0300

    Refactoring

--- widgets/LanguageDropdown.php
+++ widgets/LanguageDropdown.php
@@ -21,30 +21,10 @@ class LanguageDropdown extends Dropdown
     public function init()
     {
         $route = Yii::$app->controller->route;
-        $appLanguage = Yii::$app->language;
         $params = $_GET;
         $this->_isError = $route === Yii::$app->errorHandler->errorAction;
-
         array_unshift($params, '/' . $route);
-
-        foreach (Yii::$app->urlManager->languages as $language) {
-            $isWildcard = substr($language, -2) === '-*';
-            if (
-                $language === $appLanguage ||
-                // Also check for wildcard language
-                $isWildcard && substr($appLanguage, 0, 2) === substr($language, 0, 2)
-            ) {
-                continue;   // Exclude the current language
-            }
-            if ($isWildcard) {
-                $language = substr($language, 0, 2);
-            }
-            $params['language'] = $language;
-            $this->items[] = [
-                'label' => self::label($language),
-                'url' => $params,
-            ];
-        }
+        $this->renderLanguageItems();
         parent::init();
     }
 
@@ -72,7 +52,28 @@ class LanguageDropdown extends Dropdown
                 'en' => Yii::t('app', 'English'),
             ];
         }
-
         return isset(self::$_labels[$code]) ? self::$_labels[$code] : null;
+    }
+
+    /**
+     * Render items language select
+     */
+    public function renderLanguageItems()
+    {
+        $appLanguage = Yii::$app->language;
+        foreach (Yii::$app->urlManager->languages as $language) {
+            $isWildcard = substr($language, -2) === '-*';
+            if ($language === $appLanguage || $isWildcard && substr($appLanguage, 0, 2) === substr($language, 0, 2)) {
+                continue; // Exclude the current language
+            }
+            if ($isWildcard) {
+                $language = substr($language, 0, 2);
+            }
+            $params['language'] = $language;
+            $this->items[] = [
+                'label' => self::label($language),
+                'url' => $params,
+            ];
+        }
     }
 }
