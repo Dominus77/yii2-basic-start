5986a51288a498b4b9fb8594461c829ac4a165da
Alexey Schevchenko <ivanovosity@gmail.com>
2018-02-26 15:18:41 +0300

    Refactoring

---
+++ modules/rbac/controllers/DefaultController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace modules\rbac\controllers;
+
+use Yii;
+use yii\helpers\Url;
+use yii\filters\AccessControl;
+use yii\filters\VerbFilter;
+use modules\rbac\Module;
+
+/**
+ * Class DefaultController
+ * @package modules\rbac\controllers
+ */
+class DefaultController extends \modules\rbac\console\InitController
+{
+    /**
+     * @inheritdoc
+     * @return array
+     */
+    public function behaviors()
+    {
+        return [
+            'access' => [
+                'class' => AccessControl::class,
+                'rules' => [
+                    [
+                        'allow' => true,
+                        'roles' => ['managerRbac'],
+                    ],
+                ],
+            ],
+            'verbs' => [
+                'class' => VerbFilter::class,
+                'actions' => [
+                    'init' => YII_ENV_TEST ? ['GET'] : ['POST'],
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * Renders the index view for the module
+     * @return mixed
+     */
+    public function actionIndex()
+    {
+        return $this->render('index');
+    }
+
+    /**
+     * Переинициализация RBAC
+     * с установкой настроек по умолчанию
+     */
+    public function actionInit()
+    {
+        if ($this->processInit()) {
+            Yii::$app->session->setFlash('success', Module::t('module', 'The operation was successful!'));
+        }
+        Yii::$app->getResponse()->redirect(Url::to(['index']));
+    }
+}
--- modules/rbac/tests/_config/test-local.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return yii\helpers\ArrayHelper::merge(
-    require(__DIR__ . '/../../../../common/config/test-local.php'),
-    require(__DIR__ . '/../../../../frontend/config/test-local.php'),
-    require(__DIR__ . '/test.php'),
-    [
-    ]
-);
