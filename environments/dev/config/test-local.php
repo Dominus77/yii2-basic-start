cf6e1f8c95740eb2bac0f80d5c67378c32973f7e
Alexey Schevchenko <ivanovosity@gmail.com>
2018-04-15 12:52:09 +0300

    ch db

--- environments/dev/config/test-local.php
+++ environments/dev/config/test-local.php
@@ -4,7 +4,7 @@ return [
     'components' => [
         'db' => [
             'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=localhost;dbname=yii2basic_start_test',
+            'dsn' => 'mysql:host=localhost;dbname=yii2_basic_start_test',
             'tablePrefix' => 'tbl_',
             'username' => 'root',
             'password' => '',
