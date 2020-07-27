5b4002f7e05ff71944075272b40fe1230d4b62fb
Aleksey Schevchenko <ivanovosity@gmail.com>
2020-07-27 07:55:15 +0300

    fix phpStorm tests

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -5,3 +5,5 @@ defined('YII_DEBUG') or define('YII_DEBUG', true);
 require_once __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
 require __DIR__ .'/../vendor/autoload.php';
 require __DIR__ . '/../config/bootstrap.php';
+
+ob_start();
